5dd7a2f927238ecddb7fa01678f4775e8b3f82c1
Edgar Enuta <edgar.enuta@gmail.com>
2025-06-11 13:32:26 +0100

    Add launch configuration for debugging and refactor email authentication to load environment variables

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from retrieve_imap_mail import authenticate
+import retrieve_imap_mail as imap_mail
 
 
 if __name__ == "__main__":
-  authenticate()
-  # # user sign in
-  # try:
-  #   access_token = get_access_token()
-  #   # fetch_unread_emails(access_token)
-  # except Exception as e:
-  #   print(f"Error: {e}")
+  imap_mail.init()
+  imap_mail.authenticate()
